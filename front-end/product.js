003de3d052fa706f07aef24acb6da5658c610ef8
Chris VanGroll <chrisvangroll@gmail.com>
2021-07-24 11:12:02 -0500

    wrote JS for the varnish selector

--- front-end/product.js
+++ front-end/product.js
@@ -10,7 +10,6 @@ const id = urlParams.get('id');
 fetch('http://localhost:3000/api/furniture/' + id)
    .then(response => response.json())
    .then(data => {
-        //dropDownMenu(data);
         let item = render(data);
         document.querySelector('main').insertBefore(item, divForBtn);
 
@@ -22,7 +21,7 @@ fetch('http://localhost:3000/api/furniture/' + id)
 
   
 //Display Product 
-function render(product){
+    function render(product){
 
     let producto = document.createElement('div');
     //producto.classList.add('mx-auto');
@@ -31,26 +30,18 @@ function render(product){
             <img class = 'w-50 mx-auto' src="${product.imageUrl}" alt="furniture">
             <h3 class='itemHeader w-50 mx-auto fw-bold fs-1'>${product.name}</h3>
             <p class='description w-50 mx-auto fs-4'>${product.description}</p>
+            <div class = 'w-50 mx-auto'>
+                <label class= 'fs-4 fw-bold' for='varnish'>Choose a Varnish:</label>
+                ${dropDownMenu(product.varnish)}
+            </div>
             <div class='price w-50 mx-auto fs-4 fw-bold'>$${product.price/100} </div>
         </div>`;
 
             return producto;
     }
 
-    // function dropDownMenu (product) {
-    //     let select = document.createElement('select'); 
-    //     let options = product.varnish; 
 
-    //     for(let i = 0; i < options.length; i++) {
-    //         let opt = options[i];
-    //         let optionElement = document.createElement("option");
-    //         optionElement.textContent = opt;
-    //         optionElement.value = opt;
-    //         select.appendChild(optionElement);
-    //     }​
-    //     return select;
-    // }
-
+    
     //add to cart total and display on page
     function addToCart(){
         let productNumbers = localStorage.getItem('totalItems');
@@ -91,7 +82,20 @@ function render(product){
         }
     }
 
+    //Create dropdown menu
+    function dropDownMenu (product) {
+        let select = `<select class = 'w-25'>`; 
+        let options = product; 
+
+        for(let i = 0; i < options.length; i++) {
+            select += `<option value = '${options[i]}' class ='fs-5'>${options[i]}</option>`;
+        }
+        select += `</select>`;
+        return select;
+    }
+
+//let arr = ['one', 'two', 'three'];
+//console.log(dropDownMenu(arr))
 loadCartNumbers()
 
-    //let x = localStorage.getItem('[object Object]');
-    console.log(localStorage);
+    
